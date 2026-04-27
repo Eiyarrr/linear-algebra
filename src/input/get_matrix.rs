fec8419498ce5da42fab768d167a9569e51fa192
Eiyarrr <eiyar@eiyar.com>
2026-04-27 01:26:37 +0000

    Added logic for get_row which finishes get_matrix

--- src/input/get_matrix.rs
+++ src/input/get_matrix.rs
@@ -3,7 +3,6 @@ use std::io;
 use crate::matrix::Matrix;
 
 pub fn get_matrix() -> Matrix {
-    let mut input: String = String::from("");
     let mut data: Vec<Vec<f64>> = Vec::new();
 
     loop {
@@ -19,6 +18,30 @@ pub fn get_matrix() -> Matrix {
 
 fn get_row() -> Vec<f64> {
     let mut data: Vec<f64> = Vec::new();
+    let mut input: String = String::from("");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Could not read line.");
+
+    if input.trim().is_empty() {
+        return vec![];
+    }
+
+    let mut curr_string: String = String::new();
+    for i in input.chars() {
+        if i.is_whitespace() {
+            match curr_string.parse::<f64>() {
+                Ok(n) => data.push(n),
+                Err(e) => {
+                    println!("Could not parse \"{}\", err: {}", curr_string, e);
+                    data = vec![];
+                }
+            }
+            curr_string = String::new();
+        } else {
+            curr_string.push(i);
+        }
+    }
 
     return data;
 }
