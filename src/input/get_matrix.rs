7a8c80ee6c1d74c92371d804c8a8e7ebf2500739
Eiyarrr <eiyar@eiyar.com>
2026-04-27 01:17:59 +0000

    added get_matrix logic

--- src/input/get_matrix.rs
+++ src/input/get_matrix.rs
@@ -1,1 +1,24 @@
+use std::io;
 
+use crate::matrix::Matrix;
+
+pub fn get_matrix() -> Matrix {
+    let mut input: String = String::from("");
+    let mut data: Vec<Vec<f64>> = Vec::new();
+
+    loop {
+        let row_data = get_row();
+        if row_data == vec![] {
+            break;
+        }
+        data.push(row_data);
+    }
+
+    return Matrix::new(data);
+}
+
+fn get_row() -> Vec<f64> {
+    let mut data: Vec<f64> = Vec::new();
+
+    return data;
+}
