4c224730ff943e3a1e6f3b14fd08fac101c6f944
Eiyarrr <eiyar@eiyar.com>
2026-04-27 02:14:30 +0000

    Added files for elementary row operations

---
+++ src/computation/elementary_row_operations/mod.rs
@@ -0,0 +1,4 @@
+pub mod row_addition;
+pub mod row_multiplication;
+pub mod row_swap;
+
--- src/input/input_loop.rs
+++ src/input/input_loop.rs
@@ -1,1 +1,13 @@
+/*
+ * Get matrix
+ * get operator
+ * get matrix(?)
+ * return
+ */
 
+use crate::{input::get_matrix::get_matrix, matrix::Matrix};
+
+// temporary
+pub fn input_loop() -> Matrix {
+    return get_matrix();
+}
